706c3763db1f91b312c90eb97d20b384fbf2e0ee
姜文杰 <life.jwj@qq.com>
2021-10-21 15:25:57 +0800

    Tutorial: ch2_create_a_module

---
+++ src/ch2_create_a_module/hello/go.mod
@@ -0,0 +1,7 @@
+module ch2_create_a_module/hello
+
+go 1.17
+
+require ch2_create_a_module/greetings v1.1.0
+
+replace ch2_create_a_module/greetings => ../greetings
---
+++ src/ch2_create_a_module/hello/hello.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"ch2_create_a_module/greetings"
+	"fmt"
+	"log"
+)
+
+func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file , and line number
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(messages)
+}
